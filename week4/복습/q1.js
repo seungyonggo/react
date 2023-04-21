e20ac2e5e72d918c6731fd5d1c3e398685a72e53
seingyonggo <tmddyd432@gmail>
2023-04-21 21:59:17 +0900

    '23.04.21 q1문풀'

--- week4/복습/q1.js
+++ week4/복습/q1.js
@@ -1,5 +1,5 @@
 const user = {
-  name: '김성용',
+  name: "김성용",
   age: 20,
   height: 190,
 };
@@ -13,6 +13,18 @@ const user = {
 (4) Object.entries()
 (5) for in
 */
-
+for (let key in user) {
+  console.log(key, user[key]);
+}
 // 문제2. 값이 “김성용”인 속성의 key 찾기
+for (let key in user) {
+  if (user[key] === "김성용") {
+    console.log(key, user[key]);
+    break;
+  }
+}
 // 문제3. 깊은 복사를 통해 user 객체의 복사본을 만든 후 name을 본인의 이름으로 수정
+const clone = { ...user };
+clone.name = "고승용";
+console.log(user);
+console.log(clone);
